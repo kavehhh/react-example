d79780bd36ba5cee4f69d2d3f8d27e03118fbe18
Kaveh <kaveh@lightstep.com>
2020-07-29 15:58:37 -0700

    .

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,29 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+const request = require('request');
 var opentracing = require('opentracing');
 var lightstep   = require('lightstep-tracer');
-const request = require('request');
 
-const tracer = opentracing.initGlobalTracer(new lightstep.Tracer({
+const tracer = new lightstep.Tracer({
     access_token   : process.env.LIGHTSTEP_ACCESS_TOKEN,
     component_name : 'node-client',
-    collector_host : 'localhost',
-    collector_port : 8181,
-    collector_encryption : 'none',
+    collector_host : 'ingest.lightstep.com',
+    collector_port : 443,
+    collector_encryption : 'tls',
+    verbosity : 4,
     propagators    : {
       [opentracing.FORMAT_HTTP_HEADERS]: new lightstep.B3Propagator(),
       [opentracing.FORMAT_TEXT_MAP]: new lightstep.B3Propagator()
     }
-}));
+});
+opentracing.initGlobalTracer(tracer);
 
 function testLightstep() {
-  var span = opentracing.globalTracer().startSpan('clientStart');
+  console.log(tracer);
+
+  var span = tracer.startSpan('clientStart');
   span.log({ event : 'query_started' });
 
   var spanContext = span.context();
   var carrier = {};
-  opentracing.globalTracer().inject(spanContext, opentracing.FORMAT_HTTP_HEADERS, carrier);
+  tracer.inject(spanContext, opentracing.FORMAT_HTTP_HEADERS, carrier);
   console.log(carrier);
   //carrier['x-b3-traceid'] = carrier['x-b3-traceid'].substring(16) + carrier['x-b3-traceid'].substring(0,16)
 
